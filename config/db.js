ed9a8013899aa57bc34b030fbff8391fd0675734
JeevaCodeMaster <146964984+JeevaCodeMaster@users.noreply.github.com>
2024-05-20 13:22:00 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,1 +1,16 @@
+import mongoose from "mongoose";
+import colors from 'colors'
 
+//Replace your MONGO_URL 
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(
+            `Conneted To Mongodb Databse ${mongoose.connection.host}`.bgMagenta.white
+        );
+    } catch (error) {
+        console.log(`Error in Mongodb ${error}`.bgRed.white);
+    }
+};
+export default connectDB;
