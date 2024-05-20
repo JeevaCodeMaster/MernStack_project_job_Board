16076f68fda7a618b4ff8b6cae7532b407a1e1a6
JeevaCodeMaster <146964984+JeevaCodeMaster@users.noreply.github.com>
2024-05-20 13:59:43 +0530

    Add files via upload

--- frontend/src/components/Layout/Menus/UserMenu.js
+++ frontend/src/components/Layout/Menus/UserMenu.js
@@ -1,1 +1,17 @@
-
+export const userMenu = [
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+    icon: "fa-solid fa-house",
+  },
+  {
+    name: "Latest Jobs",
+    path: "/job/job-stats",
+    icon: "fa-solid fa-briefcase",
+  },
+  {
+    name: "Update Profile",
+    path: "/job/update-job",
+    icon: "fa-solid fa-user",
+  },
+];
